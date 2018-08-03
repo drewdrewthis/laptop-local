ad41ba74c8956bfaa17423b7f356556b80ff83b9
drew <drewdrewthis@gmail.com>
2018-08-03 10:07:59 -0400

    Create .gitignore and rename files correctly

--- cp_configs.sh
+++ cp_configs.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-echo "Copying zshrc to ~/.zshrc
+echo "Copying zshrc to ~/.zshrc"
 cp zshrc ~/.zshrc
 
-echo "Copying laptop-local to ~/.laptop-local
+echo "Copying laptop-local to ~/.laptop-local"
 cp laptop-local ~/.laptop.local
+
+echo "Copying .vimrc.local to ~/.vimrc.local"
+cp .vimrc.local ~/.vimrc.local
